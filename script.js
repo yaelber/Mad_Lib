01e3b6ed3a040384d6f301d3040d0ed25f54aa35
Yael <yaelbercow@gmail.com>
2016-06-28 09:26:21 -0400

    final commit

--- script.js
+++ script.js
@@ -4,22 +4,21 @@
 	//2. craete a function that connects the strings together
 	//3. make the button intiate the function
 	//4. shows them in a the story div 
-
-
+function makeMadLib () {
+		var madLibDisplay = document.getElementById('story');
+		var noun = document.getElementById('noun').value;
+		var adj = document.getElementById('adjective').value;
+		var person = document.getElementById('person').value;
+		var madLib = person + ' ' + adj + ' ' + noun + ' ';
+		madLibDisplay.innerText = madLib
+}
+	
 document.getElementById('lib-button').addEventListener('click', makeMadLib);
 
-function makeMadLib () {
-	var noun = document.getElementById('noun').value;
-	var adj = document.getElementById('adjective').value;
-	var person = document.getElementById('person').value;
-	var madLib = person + ' ' + adj + ' ' + noun + ' ';
-	return madLib
-
-	var madLibDisplay = document.getElementById('story');
-	madLibDisplay.innerText = madLib;
-}
-
 makeMadLib()
+
+
+
 	
 	
 
